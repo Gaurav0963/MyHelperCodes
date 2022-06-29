1a1f83fb376b56bd7b2a1ced8781d07e31b4d938
GAURAV SINGH RAJPUT <105968026+Gaurav0963@users.noreply.github.com>
2022-06-29 09:36:02 +0530

    Add files via upload

---
+++ longest word in list.py
@@ -0,0 +1,8 @@
+# FIND THE LONGEST WORD USING
+# max function and 'key' is a
+# reserved keyword which takes
+# or equates to a function
+
+my_list = ['hi', 'hey', 'hello', 'jay ''Bhole ''Nath']
+long = max(my_list, key=len)
+print(long)
